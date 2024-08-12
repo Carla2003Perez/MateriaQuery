6bd842677db91119b2705cf1ca4838e286ebbbd2
Carla Perez <=surianoc79@gmail.com>
2024-08-12 01:27:01 -0600

    creacion de contenido

---
+++ java.js
@@ -0,0 +1,19 @@
+function responsiveMenu(){
+    let x = document.getElementById("nav");
+    if(x.className===""){
+        x.className = "responsive";
+
+        //creamos el elemento que cierra el menu
+        let span = document.createElement("span");
+        span.innerHTML = "X";
+        document.getElementById("nav").appendChild(span);
+
+        //quitamos el boton eliminar cuando se hace click sobre este
+        span.onclick = function(){
+            x.className = "";
+            span.remove();
+        }
+    }else{
+        x.className="";
+    }
+}
